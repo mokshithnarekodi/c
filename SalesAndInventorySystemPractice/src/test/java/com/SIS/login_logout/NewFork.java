96d2caea5a450c8a85cca59365199fd31bd9f65d
sushant <sushantmech384@gmail.com>
2025-03-26 16:01:22 +0530

    i have adding a new class

---
+++ SalesAndInventorySystemPractice/src/test/java/com/SIS/login_logout/NewFork.java
@@ -0,0 +1,11 @@
+package com.SIS.login_logout;
+
+public class NewFork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is fork");
+
+	}
+
+}
